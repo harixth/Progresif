8b9e349426dda14955ad0fa82f410be716448e0e
Mohammad Harith <harixth@yahoo.com>
2018-06-07 09:27:17 +0800

    CRUD via API with MongoDB

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Auth/login'
 import SignUp from '@/components/Auth/SignUp'
+import Posts from '@/components/Posts/Posts'
+import NewPost from '@/components/Posts/NewPost'
+import EditPost from '@/components/Posts/EditPost'
 
 Vue.use(Router)
 
@@ -28,6 +30,16 @@ export default new Router({
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
+    },
+    {
+      path: '/posts/:id',
+      name: 'EditPost',
+      component: EditPost
     }
   ]
 })
